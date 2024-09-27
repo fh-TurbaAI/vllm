2c5e42b0187d72c26c142700f0473e6c2445888b
Florian Heilmann <florian.heilmann@turba.ai>
2024-09-27 10:01:22 +0200

    Add profiling to offline_inference.py

--- examples/offline_inference.py
+++ examples/offline_inference.py
@@ -1,20 +1,38 @@
+import torch
+from torch.profiler import ExecutionTraceObserver, profile
+
 from vllm import LLM, SamplingParams
+
+def trace_handler(prof):
+    prof.export_chrome_trace("kineto_trace.json")
 
 # Sample prompts.
 prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
     "The future of AI is",
 ]
 # Create a sampling params object.
-sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=10)
 
 # Create an LLM.
-llm = LLM(model="facebook/opt-125m")
-# Generate texts from the prompts. The output is a list of RequestOutput objects
-# that contain the prompt, generated text, and other information.
-outputs = llm.generate(prompts, sampling_params)
+llm = LLM(model="meta-llama/Meta-Llama-3.1-8B")
+
+et = ExecutionTraceObserver()
+et.register_callback("pytorch_et.json")
+et.start()
+
+with profile(
+    activities=[
+        torch.profiler.ProfilerActivity.CPU,
+        torch.profiler.ProfilerActivity.CUDA,
+    ],
+    schedule=torch.profiler.schedule(wait=0, warmup=10, active=1),
+    on_trace_ready=trace_handler
+) as prof:
+    outputs = llm.generate(prompts, sampling_params)
+
+et.stop()
+et.cleanup()
+
 # Print the outputs.
 for output in outputs:
     prompt = output.prompt
